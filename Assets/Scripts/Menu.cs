3e76d571291dd080a0e27d5342f1a436351d3d37
Oscar Nasibullin <oscarn08@gmail.com>
2017-02-20 00:38:04 +0300

    поделиться в социальные сети, пока без картинки (#7 #8 #9)

--- Assets/Scripts/Menu.cs
+++ Assets/Scripts/Menu.cs
@@ -1,4 +1,5 @@
 ﻿using UnityEngine;
+using System.IO;
 using System.Collections;
 
 public class Menu : MonoBehaviour {
@@ -22,6 +23,8 @@ public class Menu : MonoBehaviour {
 	public int maxScore = 0;
 
 	public GameObject level;
+
+	string screenshot_path;
 
 
 	bool isPlaying = false;
@@ -107,10 +110,98 @@ public class Menu : MonoBehaviour {
 	}
 
 
+//
+//
+//	public void ShareMe()
+//	{
+//		StartCoroutine( ShareScreenshot() );
+//
+//	}
+//
+//	// Сам код
+//	public IEnumerator ShareScreenshot()
+//	{
+//		// Настройки плагина
+//		string shareText  = "Game Title\n";
+//		string gameLink = " Description  "+"\nhttps://play.google.com/store/apps/details?id=???";
+//		string subject = "";
+//		string imageName = "MyPic"; // without the extension, for iinstance, MyPic 
+//
+//
+//		// wait for graphics to render
+//		yield return new WaitForEndOfFrame();
+//		//----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------- PHOTO
+//		// create the texture
+//		Texture2D screenTexture = new Texture2D(Screen.width, Screen.height,TextureFormat.RGB24,true);
+//		// put buffer into texture
+//		screenTexture.ReadPixels(new Rect(0f, 0f, Screen.width, Screen.height),0,0);
+//		// apply
+//		screenTexture.Apply();
+//		//----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------- PHOTO
+//		byte[] dataToSave = screenTexture.EncodeToPNG();
+//		string destination = Path.Combine(Application.persistentDataPath,System.DateTime.Now.ToString("yyyy-MM-dd-HHmmss") + ".png");
+//		File.WriteAllBytes(destination, dataToSave);
+//		if(!Application.isEditor)
+//		{
+//			// block to open the file and share it ------------START
+//			AndroidJavaClass intentClass = new AndroidJavaClass("android.content.Intent");
+//			AndroidJavaObject intentObject = new AndroidJavaObject("android.content.Intent");
+//			intentObject.Call<AndroidJavaObject>("setAction", intentClass.GetStatic<string>("ACTION_SEND"));
+//			AndroidJavaClass uriClass = new AndroidJavaClass("android.net.Uri");
+//			AndroidJavaObject uriObject = uriClass.CallStatic<AndroidJavaObject>("parse","file://" + destination);
+//			intentObject.Call<AndroidJavaObject>("putExtra", intentClass.GetStatic<string>("EXTRA_STREAM"), uriObject);
+//
+//			intentObject.Call<AndroidJavaObject> ("setType", "text/plain");
+//			intentObject.Call<AndroidJavaObject>("putExtra", intentClass.GetStatic<string>("EXTRA_TEXT"), shareText + gameLink);
+//			intentObject.Call<AndroidJavaObject>("putExtra", intentClass.GetStatic<string>("EXTRA_SUBJECT"), "SUBJECT");
+//
+//			intentObject.Call<AndroidJavaObject>("setType", "image/jpeg");
+//			AndroidJavaClass unity = new AndroidJavaClass("com.unity3d.player.UnityPlayer");
+//			AndroidJavaObject currentActivity = unity.GetStatic<AndroidJavaObject>("currentActivity");
+//
+//			currentActivity.Call("startActivity", intentObject);
+//		}
+//
+//	}
+//
+
+	public IEnumerator make_score_screenshot() {
+		// wait for graphics to render
+		yield return new WaitForEndOfFrame();
+		//----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------- PHOTO
+		// create the texture
+		Texture2D screenTexture = new Texture2D(Screen.width, Screen.height,TextureFormat.RGB24,true);
+		// put buffer into texture
+		screenTexture.ReadPixels(new Rect(0f, 0f, Screen.width, Screen.height),0,0);
+		// apply
+		screenTexture.Apply();
+		//----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------- PHOTO
+		byte[] dataToSave = screenTexture.EncodeToPNG();
+		screenshot_path = Path.Combine(Application.persistentDataPath,System.DateTime.Now.ToString("yyyy-MM-dd-HHmmss") + ".png");
+		File.WriteAllBytes(screenshot_path, dataToSave);
+	}
+
+
+
+	public void ShareIt (string app) {
+		string message = "My score in Trupet Saga: " + cLast.text;
+
+
+		if (app != "ok")
+			Sharing.ShareVia (app, message);
+		else
+			Sharing.ShareVia (app, message, string.Format("{0};{1}", App.OdnoklassnikiAppId, App.OdnoklassnikiSecretId));
+	}
+
+
 	public void openShareMenu() {
+//		StartCoroutine( make_score_screenshot() );
+
 		replayMenu.SetActive(false);
 		shareMenu.SetActive(true);
+
 	}
+		
 
 	public void backFromShare() {
 		replayMenu.SetActive(true);
---
+++ Assets/Sharing.cs
@@ -0,0 +1,73 @@
+﻿// vk, fb, ok, tw, wa, vb, tg
+using UnityEngine;
+using System.Collections;
+using System.Runtime.InteropServices;
+
+public class Sharing : MonoBehaviour {
+
+
+	private string vkTemplate = "http://vk.com/share.php?title={0}&description={1}&image={2}&url={3}";
+	private string facebookTemplate = "https://www.facebook.com/sharer/sharer.php?u={3}";
+	private string odnoklassnikiTemplate = "http://www.odnoklassniki.ru/dk?st.cmd=addShare&st.s=1&st._surl={3}&st.comments={1}";
+	private string twitterTemplate = "https://twitter.com/intent/tweet?text={0}&url={3}";
+
+	static string socialType;
+	static string msg;
+
+	public static string gameLink = "https://play.google.com/store/apps/details?id=???";
+
+	#if UNITY_IOS
+		[DllImport ("__Internal")]
+		private static extern void shareVia (string app, string message, string url, string param);
+	#endif
+
+	// Поделиться 
+	public static void ShareVia (string app, string message, string param = "") {
+		socialType = app;
+		msg = message;
+		message = message + "\n" + gameLink;
+		#if UNITY_ANDROID
+			using (var plugin = new AndroidJavaClass("com.mycompany.sharing.Plugin")) {
+				plugin.CallStatic("shareVia", app, message);
+			}
+		#elif UNITY_IOS
+			shareVia (app, message, "http://my.url.com", param);
+		#endif
+	}
+
+	// Не удалось расшарить
+	void OnShareError (string result) {
+		switch (result) {
+		case "NotInstall":
+			// приложение не установлено
+			Debug.Log ("приложение не установлено");
+			switch(socialType) {
+			case "vk":
+				Application.OpenURL(string.Format("http://vk.com/share.php?description={0}&url={1}", msg, gameLink));
+				break;
+
+			case "tw":
+				Application.OpenURL(string.Format("https://twitter.com/intent/tweet?text={0}&url={1}", msg, gameLink));
+				break;
+
+			case "fb":
+				Application.OpenURL(string.Format("https://www.facebook.com/sharer/sharer.php?description={0}&u={1}","",msg,gameLink));
+				break;
+			}
+
+			break;
+		case "NotAvailable":
+			// шаринг не доступен
+			Debug.Log ("шаринг не доступен");
+			break;
+		case "AccessDenied":
+			// нет доступа
+			Debug.Log ("нет доступа");
+			break;
+		default:
+			// не удалось расшарить текст
+			Debug.Log ("не удалось расшарить");
+			break;
+		}
+	}
+}
